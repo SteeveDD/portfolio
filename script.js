968cebb52aa84af687f089526315a22f6356f654
SteeveDD <steeve.desmis@gmail.com>
2024-11-01 19:59:13 +0100

    second

--- script.js
+++ script.js
@@ -6,24 +6,34 @@ gsap.from('#accueil .btn', { opacity: 0, y: -20, duration: 1, delay: 1.5 });
 gsap.from('#apropos img', { opacity: 0, x: -50, duration: 1, scrollTrigger: '#apropos' });
 gsap.from('#apropos p', { opacity: 0, x: 50, duration: 1, scrollTrigger: '#apropos' });
 
+
 // Initialisation de Swiper
 const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    // speed: 3000,
+    // autoplay: {
+    //     delay: 3000,
+    // },
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
+        rotate: 0,
+        stretch: 80,
         depth: 100,
         modifier: 1,
         slideShadows: true,
     },
-    mousewheel: true,
-    keyboard: true,
+    keyboard: {
+        enabled: false
+      },
+      mousewheel: {
+        thresholdDelta: 70
+      }
 });
 
-// Animation GSAP pour les projets
+// // Animation GSAP pour les projets
 gsap.from('.swiper-slide', {
     opacity: 0,
     y: 50,
@@ -35,7 +45,7 @@ gsap.from('.swiper-slide', {
     }
 });
 
-// Smooth scrolling
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -53,10 +63,24 @@ const chatForm = document.getElementById('chat-form');
 const chatInput = document.getElementById('chat-input');
 const chatMessages = document.getElementById('chat-messages');
 
+chatBody.style.display = 'none'
+
 chatToggle.addEventListener('click', () => {
     chatBody.style.display = chatBody.style.display === 'none' ? 'flex' : 'none';
     chatToggle.textContent = chatBody.style.display === 'none' ? '+' : '-';
 });
+
+
+setTimeout(() => {
+    chatBody.style.display = 'flex';
+    chatToggle.textContent = '-'; // Mettre à jour le texte du bouton
+
+    // Envoyer le message par défaut après 1 seconde de l'ouverture
+    setTimeout(() => {
+        addMessage('bot', "Bonjour, je m'appel Nestor, je suis le major d'homme de ce portfolio. Comment puis-je vous aider aujourd'hui ?");
+    }, 1000);
+}, 10000);
+
 
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
